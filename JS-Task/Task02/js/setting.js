d721252b7202c91e202d1b43ff164fcede25acfa
zhuangyin8 <zhuangyin8@gmail.com>
2017-04-19 16:37:07 +0800

    update js-task02

--- JS-Task/Task02/js/setting.js
+++ JS-Task/Task02/js/setting.js
@@ -18,19 +18,19 @@ var btnAdd = document.getElementById("btnAdd");
  */
 rangeNum.onchange = function rangeChange() {
     textNum.value = rangeNum.value;
-    // 文本输入框和人数设置拖动条联动
+    //将range的值给text
 }
 ;
 /*
-*
-* 对玩家人数进行判断
-*
-* onblur 事件 当元素失去焦点
-* 对输入框的数字失去焦点,判断是否符合要求
-* 如果不符合要求(玩家人数<4),提示用户并将玩家人数自动设置为4
-* 如果不符合要求(玩家人数>18),提示用户并将玩家人数自动设置为4
-* 如果符合要求(4<玩家人数<18),将将人数设置拖动条的值赋值给输入框
-*/
+ *
+ * 对玩家人数进行判断
+ *
+ * onblur 事件 当元素失去焦点
+ * 对输入框的数字失去焦点,判断是否符合要求
+ * 如果不符合要求(玩家人数<4),提示用户并将玩家人数自动设置为4
+ * 如果不符合要求(玩家人数>18),提示用户并将玩家人数自动设置为4
+ * 如果符合要求(4<玩家人数<18),将将人数设置拖动条的值赋值给输入框
+ */
 textNum.onblur = function textChange() {
     if (textNum.value < 4) {
         alert("人数少于4人不能开始游戏哦");
@@ -68,22 +68,20 @@ btnAdd.onclick = function btnAdd() {
 ;
 
 /*
-将杀手和平民身份打乱，准备分配给1-N号玩家身份
-*/
+ 将杀手和平民身份打乱，准备分配给1-N号玩家身份
+ */
 var identifyShow = document.getElementById("identifyShow");
 //获取显示玩家身份区域的值
 var identifyGet = document.getElementById("identifyGet");
 //获取点击设置按钮的值
 var allPlayer;
 var player = "";
-//点击"点击设置按钮"
 identifyGet.onclick = function identifyGet() {
     player = "";
-    //清空原来显示区域 
+    //清空显示区域的元素
     var killer = [];
     //杀手数组的声明
-    var civy = [];
-
+    var person = [];
     //平民数组的声明
     if (textNum.value >= 4 && textNum.value <= 8) {
         //不同人数范围设置不同的杀手数量
@@ -98,30 +96,28 @@ identifyGet.onclick = function identifyGet() {
         alert('人数不满足条件，请重新设置人数哦');
         //之前已经判断人数后，几乎不可能出现，但是为了保险起见
     }
-    // for (var i = 0; i < killer.length; i++) {
-    //     //生成杀手数组并在控制台打印
-    //     killer[i] = "杀手";
-    //     console.log(killer[i]);
-    // }
-    // console.log(killer);
-    // for (var j = 0; j < textNum.value - killer.length; j++) {
-    //     //生成平民数组并在控制台打印
-    //     civy[j] = "平民";
-    //     console.log(civy[j]);
-    // }
+    for (var i = 0; i < killer.length; i++) {
+        //生成杀手数组并在控制台打印
+        killer[i] = "杀手";
+        console.log(killer[i]);
+    }
+    console.log(killer);
+    for (var j = 0; j < textNum.value - killer.length; j++) {
+        //生成平民数组并在控制台打印
+        person[j] = "平民";
+        console.log(person[j]);
+    }
 
     //将杀手和平民的数组合并并将数组的顺序打乱后输出
-    var all = killer.concat(civy);    //Array.prototype.concat() 返回一个由当前数组和其它若干个数组或者若干个非数组值组合而成的新数组。
+    var all = killer.concat(person);
 
-    // function randomSort(a, b) {
-    //     return Math.random() > .5 ? -1 : 1;
-    // }
-
-    all.sort(function () {
+    function randomSort(a, b) {
         return Math.random() > .5 ? -1 : 1;
-    });
+    }
+
+    all.sort(randomSort);
     //利用随机函数产生一个随机数字，再传入sort()取得随机打乱的数组
-    // console.log(all);
+    console.log(all);
     for (var m = 0; m < all.length; m++) {
         //输出打乱的数组，即分配玩家身份，且在页面中显示
         if (all[m] == "杀手") {
@@ -131,7 +127,7 @@ identifyGet.onclick = function identifyGet() {
         }
         console.log(player);
         identifyShow.innerHTML = player;
-        //设置页面显示区域的html
+        //设置显示区域的html
     }
     allPlayer = JSON.stringify(all);
     //使用json把js对象转换为字符串
